420ca8336190cc6b819f33eaf0364eae592bf664
Francisco Rojas <frojas@alumnos.utalca.cl>
2013-07-09 03:41:06 +0000

    render index.html instead of hardcoded greeting

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  out = fs.readFileSync('views/index.html');
+  response.send(out.toString());
 });
 
 var port = process.env.PORT || 5000;
