2d1ee420bb4ef59adf0884be2271291c02a4f4ef
Francisco Rojas <frojas@alumnos.utalca.cl>
2013-07-10 00:46:48 +0000

    extend grader to check html from url

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "views/index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,8 +37,8 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(stream) {
+    return cheerio.load(stream.toString());
 };
 
 var loadChecks = function(checksfile) {
@@ -61,14 +62,32 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+var pr = function(program, stream) {
+    var checkJson = checkHtmlFile(stream, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <uri>', 'Url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var stream;
+    if (program.url) {
+        rest.get(program.url).on('complete', function(data) {
+            stream = data;
+            pr(program, stream);
+        });
+    } else {
+        stream = fs.readFileSync(program.file);
+        pr(program, stream);
+    }
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
