3ce3881cf7471c77fa79928607f5fa10007d710a
Francisco Rojas <frojas@groupon.com>
2013-08-06 23:12:38 -0400

    remove dup index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  out = fs.readFileSync('views/index.html');
+  out = fs.readFileSync('index.html');
   response.send(out.toString());
 });
 
